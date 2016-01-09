c90483aef33c7c697e3eb459fd581b8be29bbfab
dan-dan <danielhc2@gmail.com>
2016-01-09 09:58:42 -0800

    Initial git commit

---
+++ watchmedrop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watchmedrop
+//
+//  Created by Daniel Cortez on 1/9/16.
+//  Copyright © 2016 HCD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
