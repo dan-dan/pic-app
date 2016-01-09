a51e511eb69c6c9a1116b0bcfc766c6b496c814c
dan-dan <danielhc2@gmail.com>
2016-01-09 10:18:25 -0800

    Measurement model added

---
+++ watchmedrop/Measurement.swift
@@ -0,0 +1,19 @@
+//
+//  Measurement.swift
+//  watchmedrop
+//
+//  Created by Daniel Cortez on 1/9/16.
+//  Copyright © 2016 HCD. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Measurement {
+    
+    var date = NSDate()
+    var weight = 0.0
+    var bodyFatPercentage = 0.0
+    var image : UIImage?
+
+}
