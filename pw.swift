995ba2d9c1603637dd303951385070266c682f74
dan-dan <danielhc2@gmail.com>
2016-01-09 10:56:17 -0800

    Added Measurement Model

---
+++ pw.swift
@@ -0,0 +1,11 @@
+//
+//  pw.swift
+//  watchmedrop
+//
+//  Created by Daniel Cortez on 1/9/16.
+//  Copyright © 2016 HCD. All rights reserved.
+//
+
+import Foundation
+
+let kWatchMeDropTemporaryPassword = "3n3rG1z3r13UnniY"
